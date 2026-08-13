1d07068fcb20baf2a6891612a1b5cf3029fccaf8
wblee0703 <wblee0703@users.noreply.github.com>
2026-08-13 08:00:48 +0000

    deploy: 402889c54e83b26417d49d0365f550fe39142fb2

--- assets/web-CqVLtcU3.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-D_GFWQ5e.js";class p extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{p as AppLauncherWeb};
