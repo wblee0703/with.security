596a3d70900c3a20876ad315745088cf64a38db6
wblee0703 <wblee0703@users.noreply.github.com>
2026-08-14 16:54:39 +0000

    deploy: 8f38b7c91ec92e74bca3914f3f8a47fdd1f801a7

---
+++ assets/web-B3hp0jsO.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-le5AUdgb.js";class p extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{p as AppLauncherWeb};
