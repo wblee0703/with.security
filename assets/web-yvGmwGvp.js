7cdcdae0837574134b77953d2c7be7a275c99797
wblee0703 <wblee0703@users.noreply.github.com>
2026-08-18 07:55:04 +0000

    deploy: 8846e479c8c6ac39337876adbf5fb2af0450dad6

---
+++ assets/web-yvGmwGvp.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-QOiCHO5D.js";class p extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{p as AppLauncherWeb};
