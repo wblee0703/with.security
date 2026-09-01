50e06d3953829c48efe6eb0cc6e44f6c7bef40a0
wblee0703 <wblee0703@users.noreply.github.com>
2026-09-01 15:02:39 +0000

    deploy: 67b2e6d8706c527fa815f72ac0983a6dc506ea68

--- assets/web-DbA0xg38.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-g8Z0haGh.js";class p extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{p as AppLauncherWeb};
